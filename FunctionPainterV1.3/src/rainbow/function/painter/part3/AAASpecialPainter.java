3eb6b68ed731b9a407e85ac5c19eb613fe18c132
Rainbow_Yang <1041410345@qq.com>
2016-07-29 16:46:43 +0800

    AAA完成

--- FunctionPainterV1.3/src/rainbow/function/painter/part3/AAASpecialPainter.java
+++ FunctionPainterV1.3/src/rainbow/function/painter/part3/AAASpecialPainter.java
@@ -9,5 +9,28 @@ public class AAASpecialPainter {
 	}
 
 	public void paintImg(Function f, String[] parts, int mode) {
+		// a*x^m+b*x^n+c*x^q=0 -> a*x(m-q)+b*x^(n-q)+c=0
+		int a = Integer.parseInt(parts[0].substring(0, parts[0].indexOf("*")));
+		int m = Integer.parseInt(parts[0].substring(parts[0].indexOf("^") + 1));
+		int b = Integer.parseInt(parts[1].substring(0, parts[1].indexOf("*")));
+		int n = Integer.parseInt(parts[1].substring(parts[1].indexOf("^") + 1));
+		int c = Integer.parseInt(parts[2].substring(0, parts[2].indexOf("*")));
+		int q = Integer.parseInt(parts[2].substring(parts[2].indexOf("^") + 1));
+
+		switch (mode) {
+		case 0:
+			parts[0] = a + "*x^" + (m - q);
+			parts[1] = b + "*x^" + (n - q);
+			parts[2] = c + "";
+
+			break;
+		case 1:
+			parts[0] = a + "";
+			parts[1] = b + "*y^" + (n - m);
+			parts[2] = c + "*y^" + (q - m);
+
+			break;
+		}
+		new AAOSpecialPainter(f, parts, mode);
 	}
 }
