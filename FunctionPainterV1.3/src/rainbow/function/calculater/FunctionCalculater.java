3b80056acb15d6ac87f9eb140d1c4b4c1f682dcb
Rainbow_Yang <1041410345@qq.com>
2016-07-29 16:55:36 +0800

    删除class

--- FunctionPainterV1.3/src/rainbow/function/calculater/FunctionCalculater.java
+++ FunctionPainterV1.3/src/rainbow/function/calculater/FunctionCalculater.java
@@ -11,13 +11,17 @@ import rainbow.function.FunctionPointsPainter;
  */
 public class FunctionCalculater {
 	private FunctionPointsPainter fpp;
+	private Function f;
 
 	public FunctionCalculater(Function f) {
+		this.f=f;
 		fpp = new FunctionPointsPainter(f);
 		new FunctionCalculaterThread(f,fpp,1).start();
 		new FunctionCalculaterThread(f,fpp,2).start();
 		new FunctionCalculaterThread(f,fpp,3).start();
 		new FunctionCalculaterThread(f,fpp,4).start();
 	}
+	
+	private 
 
 }
